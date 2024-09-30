a838840430aad9992cd7e232920bf6b3ab7c2002
chayma550 <rachayma@gmail.com>
2024-09-30 18:05:01 +0200

    Update CORS settings

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "https://fiveer-app.onrender.com/",
+  baseURL: "https://fiveer-app.onrender.com/api/",
   withCredentials: true,
 });
 
